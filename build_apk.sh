6f75e33e9908ec24d278bca73f1959669a8df298
fangju <fangju@wepie.com>
2021-07-16 11:03:34 +0800

    修改构建脚本

--- build_apk.sh
+++ build_apk.sh
@@ -2,9 +2,17 @@
 
 echo "###############开始构建#################"
 buildType=$1
-echo $buildType
+buildBranch=$2
+buildNotify=$3
+
+echo "buildType: ${buildType}"
+echo "buildBranch: ${buildBranch}"
+echo "buildNotify: ${buildNotify}"
 
 cd ${WORKSPACE}
 ls
 ./gradlew clean assemble${buildType} --stacktrace
+
+
+
 echo "###############构建结束#################"
